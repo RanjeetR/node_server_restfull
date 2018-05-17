e9d1833bdf2d5d47dfd5c4f472cd8c458d9f1b53
RanjeetR <ranjeet.hange@outlook.com>
2018-05-17 14:08:59 +0530

    mlab added

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,21 @@
 const products = require('express').Router({ mergeParams: true });
 var fs = require('fs');
+const mongoose = require('mongoose');
 
 
 var path = require('path');
 var appDir = path.dirname(require.main.filename);
 
 module.exports = (req, res) => {  
-  fs.readFile( appDir +'/data/product.json', 'utf-8', (err, data)=> {
-    res.end( data );
-  })
-    
+  var dburl = 'mongodb://RanjeetR:Ranjeet_1989@ds125680.mlab.com:25680/ranjeetdb';
+mongoose.connect(dburl, function(err, db) {
+    if (err) throw err;
+    db.collection("productdetails").findOne({}, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+      db.close();
+    });
+  });    
   };
+
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var cors = require('cors')
+var cors = require('cors');
+
 const routes = require('./routes');
+app.use(cors());
 // var corsOptions = {
 //     origin: 'http://localhost:8080',
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 //   }
-  
-app.use(cors());
 
 app.use('/', routes);
-var server = app.listen(process.env.PORT  , function(){
+var server = app.listen(process.env.PORT, function(){
     console.log('server started at ' + server.address().port);
 });
